0b3bbd460ab1e20964ad3540b142931785964670
alhera <alhernandezriv@hotmail.com>
2021-06-27 01:49:09 -0600

    Added modal function for loading elements data

--- views/alojamientoView.php
+++ views/alojamientoView.php
@@ -47,14 +47,74 @@ $criterios = [
     <div class="container d-flex h-100 align-items-center mx-auto">
         <?php
             include_once 'views/barraCriterios.php';
-
+            $counter = 0;
+            $selectedValue = 0;
         ?>
     </div>
 </header>
 <br>
 <br>
 
-
+<div class="modal fade bd-example-modal-lg" id="example" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div id="carouselHolidayIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselHolidayIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselHolidayIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselHolidayIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img id="img1" class="d-block w-100" src="" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img id="img2" class="d-block w-100" src="" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <div class="video-responsive">
+                                <iframe id="video1" width="560" height="315" src="https://www.youtube.com/embed/f0bc5E24Js4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselHolidayIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselHolidayIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                <br>
+                <div>
+                    <p id="descripcion">
+                        Se ofrece wifi gratuito a los huéspedes y las habitaciones de Holiday Inn Express
+                        San Jose Airport incluyen televisor de pantalla plana y aire acondicionado.
+                        <br><br>
+                        Durante la estancia, disfruta de algunos de los servicios que ofrecen, como recepción abierta 24 horas,
+                        servicio de habitaciones y conserje. Los huéspedes de Hotel Holiday Inn Express San Jose Airport también
+                        disponen de piscina y desayuno incluido allí mismo. Para viajeros con coche hay parking gratis disponible.
+                        <br><br>
+                        Para aquellos que estén interesados en visitar puntos de referencia conocidos durante su viaje a
+                        Alajuela, Hotel Holiday Inn Express San Jose Airport se encuentra cerca de Teatro Municipal
+                        (2,6 km) y Our Lady of the Pillar Cathedral (2,7 km).
+                    </p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary data-dismiss" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <section id="result">
     <div style="" class="container" id="content">
@@ -76,26 +136,34 @@ $criterios = [
                                     <h5 class="card-title"><?= $result['nombre'] ?></h5>
                                     <p class="card-text"><?= $result['descripcion'] ?></p>
 
-                                    <form id="<?= "alojamiento" . $result['id'] ?>" name="selected" method="post">
-                                        <input name="selected_id" type="hidden" placeholder="Type" id="<?= "input" . $result['id'] ?>" value="<?= $result['id'] ?>">
-                                        <a type="button" id="<?= "link" . $result['id'] ?>" onclick="<?= "getSelectedValue" . $result['id'] . "()"?>">Ver más</a>
-                                    </form>
+<!--                                    <form id="--><?//= "alojamiento" . $result['id'] ?><!--" name="selected" method="post">-->
+<!--                                        <input name="selected_id" type="hidden" placeholder="Type" id="--><?//= "input" . $result['id'] ?><!--" value="--><?//= $result['id'] ?><!--">-->
+<!--                                        <a type="button" id="--><?//= "link" . $result['id'] ?><!--" onclick="--><?//= "getSelectedValue" . $result['id'] . "()"?><!--">Ver más</a>-->
+<!--                                    </form>-->
 
+                                    <a type="button" id="<?= "link_" . $result['id'] ?>" onclick="getSelectedValue<?=$result['id'] ?>()">Ver más</a>
                                     <script>
 
-
-                                        function until(conditionFunction) {
-
-                                            const poll = resolve => {
-                                                if(conditionFunction()) resolve();
-                                                else setTimeout(_ => poll(resolve), 400);
-                                            }
-                                            return new Promise(poll);
-                                        }
-
-                                        async function getSelectedValue<?= $result['id'] ?>() {
-                                            document.getElementById("<?= "alojamiento" . $result['id'] ?>").submit();
+                                        function getSelectedValue<?=$result['id'] ?>() {
+                                            let nombre = "<?= $result['nombre'] ?>";
+                                            let descripcion = "<?= $result['descripcion'] ?>";
+                                            let urlImagen1 = "<?= $result['url_imagen_1'] ?>";
+                                            let urlImagen2 = "<?= $result['url_imagen_2'] ?>";
+                                            let urlVideo = "<?= $result['url_video'] ?>";
+                                            //const urlMapa = "<?//= $result['url_mapa '] ?>//";
+                                            //alert(nombre);
                                             $("#example").modal("show");
+
+                                            let inputF = document.getElementById("exampleModalLongTitle");
+                                            let paragElem = document.getElementById("descripcion");
+                                            let imgElem = document.getElementById("img1");
+                                            let img2Elem = document.getElementById("img2");
+                                            let videoElem = document.getElementById("video1");
+                                            inputF.textContent = nombre;
+                                            paragElem.textContent = descripcion;
+                                            imgElem.setAttribute("src", urlImagen1);
+                                            img2Elem.setAttribute("src", urlImagen2);
+                                            videoElem.setAttribute("src", urlVideo);
                                         }
                                     </script>
 
@@ -105,6 +173,7 @@ $criterios = [
                     </div>
                 </li>
                 <?php
+                    $counter++;
                 }
             ?>
         </ul>
@@ -112,66 +181,6 @@ $criterios = [
 </section>
 
 
-<div class="modal fade bd-example-modal-lg" id="example" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div id="carouselHolidayIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/a1/78/74/breakfast-bar.jpg?w=1200&h=-1&s=1" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/a1/78/6b/swimming-pool.jpg?w=1200&h=-1&s=1" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <div class="video-responsive">
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/f0bc5E24Js4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselHolidayIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselHolidayIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                <br>
-                <div>
-                    <p>
-                        Se ofrece wifi gratuito a los huéspedes y las habitaciones de Holiday Inn Express
-                        San Jose Airport incluyen televisor de pantalla plana y aire acondicionado.
-                        <br><br>
-                        Durante la estancia, disfruta de algunos de los servicios que ofrecen, como recepción abierta 24 horas,
-                        servicio de habitaciones y conserje. Los huéspedes de Hotel Holiday Inn Express San Jose Airport también
-                        disponen de piscina y desayuno incluido allí mismo. Para viajeros con coche hay parking gratis disponible.
-                        <br><br>
-                        Para aquellos que estén interesados en visitar puntos de referencia conocidos durante su viaje a
-                        Alajuela, Hotel Holiday Inn Express San Jose Airport se encuentra cerca de Teatro Municipal
-                        (2,6 km) y Our Lady of the Pillar Cathedral (2,7 km).
-                    </p>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary data-dismiss" data-dismiss="modal">Cerrar</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 </body>
 
