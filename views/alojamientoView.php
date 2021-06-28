38fa94dc6511d8d9d486c4502e8574dbfb19186f
alhera <alhernandezriv@hotmail.com>
2021-06-27 19:17:58 -0600

    Added modularity to bootstrap modal. There is only one general modal now for all to use

--- views/alojamientoView.php
+++ views/alojamientoView.php
@@ -38,7 +38,34 @@ $criterios = [
         "Más de 5 días" => 5
     ],
     "Habitaciones" => 0
-]
+];
+
+    $aditionalContent = '<div class="row"><h5>Otras Caracteristicas Adicionales</h5></div>
+
+                    <div class="row">
+                        <div class="col-sm">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <img id="img-aire" width="5%" src="">&nbsp;&nbsp;Aire Acondicionado
+                                </li>
+                                <li class="list-group-item">
+                                    <img id="img-taxi" width="5%" src="">&nbsp;&nbsp;Servicio de Taxi
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-sm">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <img id="img-tele" width="5%" src="">&nbsp;&nbsp;Television
+                                </li>
+                                <li class="list-group-item">
+                                    <img id="img-ducha" width="5%" src="">&nbsp;&nbsp;Baño / Ducha
+                                </li>
+                            </ul>
+                        </div>
+                    </div>';
+
 ?>
 
 <body>
@@ -53,99 +80,9 @@ $criterios = [
 <br>
 <br>
 
-<div class="modal fade bd-example-modal-lg" id="example" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div id="carouselHolidayIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselHolidayIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img id="img1" class="d-block w-100" src="" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img id="img2" class="d-block w-100" src="" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <div class="video-responsive">
-                                <iframe id="video1" width="100%" height="100%" src="https://www.youtube.com/embed/f0bc5E24Js4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselHolidayIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselHolidayIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                <br>
-                <div>
-                    <p id="descripcion"></p>
-                </div>
-
-                <div class="box">
-                    <div class="box-sm red"></div>
-                    <div class="box-sm orange"></div>
-                    <div class="box-sm yellow "></div>
-                    <div class="box-sm green "></div>
-                    <div class="box-sm blue "></div>
-                    <div class="box-sm purple"></div>
-                </div>
-
-                <div class="container">
-                    <div class="row"><h5>Otras Caracteristicas Adicionales</h5></div>
-
-                    <div class="row">
-                        <div class="col-sm">
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">
-                                    <img id="img-aire" width="5%" src="<?= true ? "../assets/img/check.svg" : "../assets/img/crossed.svg"; ?>" alt="icon-air-conditioner">&nbsp;&nbsp;Aire Acondicionado
-                                </li>
-                                <li class="list-group-item">
-                                    <img id="img-taxi" width="5%" src="../assets/img/crossed.svg" alt="icon-air-conditioner">&nbsp;&nbsp;Servicio de Taxi
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="col-sm">
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">
-                                    <img id="img-tele" width="5%" src="../assets/img/check.svg" alt="icon-air-conditioner">&nbsp;&nbsp;Television
-                                </li>
-                                <li class="list-group-item">
-                                    <img id="img-ducha" width="5%" src="../assets/img/check.svg" alt="icon-air-conditioner">&nbsp;&nbsp;Baño / Ducha
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="row" style="padding-top: 5%">
-                        <h5>Ubicación</h5>
-                        <iframe id="mapa1" src="" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                    </div>
-
-                </div>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary data-dismiss" data-dismiss="modal">Cerrar</button>
-            </div>
-        </div>
-    </div>
-</div>
+<?=
+    include_once 'views/generalModal.php'
+?>
 
 <section id="result">
     <div style="" class="container" id="content">
@@ -185,7 +122,7 @@ $criterios = [
                                             let banoDucha = "<?= $result['baño_ducha'] ?>";
 
                                             // Selects the html elements that are needed to load the result data
-                                            let inputF = document.getElementById("exampleModalLongTitle");
+                                            let inputF = document.getElementById("modal-title");
                                             let paragElem = document.getElementById("descripcion");
                                             let imgElem = document.getElementById("img1");
                                             let img2Elem = document.getElementById("img2");
