a711f9c9f131be8db4a65c516a2bab0cf2cf8951
Jean Carlo Valenciano <jeancavm1303@hotmail.com>
2021-06-29 15:18:36 -0600

    Vehiculo frontend y base de datos

--- controllers/vehiculosController.php
+++ controllers/vehiculosController.php
@@ -1,8 +1,21 @@
 <?php
 
+    require_once "core/Vehiculo.php";
+    require_once "core/Request.php";
     class vehiculosController {
 
         public function vehiculosAction() {
+
+        $vehiculo = new Vehiculo();
+        $request = new Request();
+        $results = [];
+
+        if(strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
+
+            $vehiculo->setData($request->getBody()); // Get data from user from UI
+            $results = $vehiculo->getDbData(); // Get data from data base
+        }
+
             require_once "views/vehiculosView.php"; 
         } 
     }
---
+++ core/Vehiculo.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once "configuration/database.php";
+
+
+class Vehiculo
+{
+    const PASAJEROS = 'pasajeros';
+    const TIEMPO = 'tiempo';
+    const PRECIO = 'precio';
+
+    public int $pasajeros = 0;
+    public int $tiempo = 0;
+    public float $precio = 0;
+    public array $values = [];
+
+    public function __construct()
+    {
+        $this->values = [
+            self::PASAJEROS => $this->pasajeros,
+            self::TIEMPO => $this->tiempo,
+            self::PRECIO => $this->precio,
+        ];
+    }
+
+    // assigns user input to model attributes
+    public function setData($data) {
+        foreach ($data as $key => $value) {
+            if(property_exists($this, $key)) {
+                $this->{$key} = (int) $value;
+            }
+        }
+
+        $this->values = [
+            self::PASAJEROS => $this->pasajeros,
+            self::TIEMPO => $this->tiempo,
+            self::PRECIO => $this->precio,
+        ];
+        return $this->values;
+    }
+
+    // gets data from database
+    public function getDbData() {
+
+        $resultData = [];
+
+        $conn = \app\configuration\Conectar::conexion2();
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "CALL determinar_probabilidades_vehiculo($this->precio, $this->tiempo, $this->pasajeros);";
+        $result = $conn->query($sql);
+
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            array_push($resultData, $row);
+        }
+
+        $conn->close();
+
+        return $resultData;
+    }
+
+
+}
